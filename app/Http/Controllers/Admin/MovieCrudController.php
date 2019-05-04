27ef141ffa767ca074b5521e505c79344d671d7e
josuebraigo <josue@braigo.mx>
2019-05-04 16:59:58 -0500

    Commit a medias

--- app/Http/Controllers/Admin/MovieCrudController.php
+++ app/Http/Controllers/Admin/MovieCrudController.php
@@ -168,7 +168,7 @@ class MovieCrudController extends CrudController
     public function store(StoreRequest $request)
     {
         //dd($request->request);
-        $request->request->set('slug', 'slug');
+        // $request->request->set('slug', 'slug');
         // your additional operations before save here
         $redirect_location = parent::storeCrud($request);
         // your additional operations after save here
--- database/seeds/GenresTableSeeder.php
+++ database/seeds/GenresTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Models\Genres;
+use App\Models\Genre;
 
 class GenresTableSeeder extends Seeder
 {
@@ -13,87 +13,87 @@ class GenresTableSeeder extends Seeder
     public function run()
     {
         //
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Acción";
         $genres->slug = "accion";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Animación";
         $genres->slug = "animacion";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Aventura";
         $genres->slug = "aventura";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Bélico";
         $genres->slug = "belico";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Ciencia Ficción";
         $genres->slug = "ciencia-ficcion";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Cine Negro";
         $genres->slug = "cine-negro";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Comedia";
         $genres->slug = "comedia";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Desconocido";
         $genres->slug = "desconocido";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Documental";
         $genres->slug = "documental";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Drama";
         $genres->slug = "drama";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Fantástico";
         $genres->slug = "fantastico";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Infantil";
         $genres->slug = "infantil";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Intriga";
         $genres->slug = "intriga";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Musical";
         $genres->slug = "musical";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Romance";
         $genres->slug = "romance";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Terror";
         $genres->slug = "terror";
         $genres->save();
 
-        $genres = new Genres();
+        $genres = new Genre();
         $genres->name = "Thriller";
         $genres->slug = "thriller";
         $genres->save();
