44fe636994d09b307cead8945bd4ae16d399271e
josuebraigo <josue@braigo.mx>
2019-05-19 18:00:01 -0500

    Json de asientos

--- public/js/general.js
+++ public/js/general.js
@@ -13,55 +13,59 @@ module.exports = __webpack_require__(9);
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function($) {$(document).ready(function () {
-				$('.peliculas').slick({
-								infinite: true,
-								slidesToShow: 4,
-								slidesToScroll: 1,
-								autoplay: true,
-								autoplaySpeed: 3000,
-								responsive: [{
-												breakpoint: 1024,
-												settings: {
-																slidesToShow: 3,
-																slidesToScroll: 3,
-																infinite: true,
-																dots: true
-												}
-								}, {
-												breakpoint: 600,
-												settings: {
-																slidesToShow: 2,
-																slidesToScroll: 2
-												}
-								}, {
-												breakpoint: 480,
-												settings: {
-																slidesToShow: 1,
-																slidesToScroll: 1
-												}
-								}]
-				});
+	$('.peliculas').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			}
+		}, {
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
 
-				$('.container .submenu h4').click(function () {
-								$(this).siblings('ul').slideToggle();
-								if ($(this).hasClass('active')) {
-												$(this).removeClass('active');
-								} else {
-												$(this).addClass('active');
-								}
-				});
+	$('.container .submenu h4').click(function () {
+		$(this).siblings('ul').slideToggle();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+		} else {
+			$(this).addClass('active');
+		}
+	});
 
-				var index;
+	var index;
+	// var seats = {!! json_decode($seats->seats) !!};
+	// var funcion = {{ App\Models\Funcion::find($seats->function_id) }};
+	// console.log(seats);
+	// console.log(funcion);
 
-				$('.asientos .seat').click(function () {
-								if (!$(this).hasClass('active')) {
-												$(this).addClass('active');
-												index = $('span').index(this);
-												alert(index);
-								} else {
-												$(this).removeClass('active');
-								}
-				});
+	$('.asientos .seat').click(function () {
+		if (!$(this).hasClass('active')) {
+			$(this).addClass('active');
+			index = $('span').index(this);
+			alert(index);
+		} else {
+			$(this).removeClass('active');
+		}
+	});
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
--- resources/views/seats.blade.php
+++ resources/views/seats.blade.php
@@ -24,109 +24,28 @@
     </header>
     <div class="container">
       <h1>Elige tus asientos</h1>
-      <div class="asientos"><span class="seat"><i class="fa fa-star"></i>
-          <p>A 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>A 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>B 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>C 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>D 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>E 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>F 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>G 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>H 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>I 9</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 0</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 1</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 2</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 3</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 4</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 5</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 6</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 7</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 8</p></span><span class="seat"><i class="fa fa-star"></i>
-          <p>J 9</p></span>
+      <div class="asientos">
+        @php
+          {{$y = 0;}}
+          {{$arr = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');}}
+          {{$asientos=json_decode($seats[0])->seats;}}
+        @endphp
+        @for($x = 0; $x < 100; $x++)
+          @if($asientos[$x] == 'false')<span class="seat"><i class="fa fa-star"></i>
+            <p>{{$y}}</p></span>
+          @elseif($asientos[$x] == 'true')<span class="seat ocupado"><i class="fa fa-star"></i>
+            <p>{{$y}}</p></span>
+          @endif
+          @php
+            {{$y++;}}
+          @endphp
+          @if($y == 9)
+            {{ $y = 0 }}
+          @endif
+        @endfor
       </div>
       <button type="submit">Continuar</button>
+      <p hidden>{{ json_decode($seats[0])->seats }}</p>
     </div>
     <footer><a href="#">Metrocinemas</a><a href="#">Aviso de privacidad</a>
       <script src="{{ mix('/js/manifest.js') }}" type="text/javascript"></script>
